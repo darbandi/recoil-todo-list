786d1330e9d883bc621ee4402ddddb06a4c51bbe
Mohammad Darbandi <darbandi.craft@gmail.com>
2022-05-17 07:53:49 +0430

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Input} from './components/Input'
+import {Stats} from './components/Stats'
+import {Tasks} from './components/Tasks'
+import {ThemeProvider, GlobalStyles, Page} from './components/theme'
+import {darkModeState, Header} from './components/Header'
+import { useRecoilValue } from 'recoil'
+
+const Home = () => {
+    return (
+        <Page>
+            <Header />
+            <Stats />
+            <Tasks />
+            <Input />
+        </Page>
+    )
+}
+
+const App = () => {
+    const darkMode = useRecoilValue(darkModeState);
+    return (
+        <ThemeProvider darkMode={darkMode}>
+            <GlobalStyles />
+            <Home />
+        </ThemeProvider>
+    )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {atom, useRecoilState} from 'recoil'
+import styled from 'styled-components'
+import {Heading} from './Heading'
+import {Switch} from './Switch'
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 30px 0;
+`
+
+export const darkModeState = atom({
+    key: 'darkMode',
+    default: false,
+})
+
+export const Header: React.FC = () => {
+    const [darkMode, setDarkMode] = useRecoilState(darkModeState)
+
+    return (
+        <Container>
+            <Heading />
+            <Switch value={darkMode} onChange={setDarkMode} />
+        </Container>
+    )
+}
